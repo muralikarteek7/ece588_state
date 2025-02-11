f1cd3c1a50f233229db6d587d3d151f1ba27c2df
xTheQAZx <dhruv.agarwals@gmail.com>
2025-02-10 21:47:45 -0600

    add process_oak functionality

--- oak_2_ouster_transformation.py
+++ oak_2_ouster_transformation.py
@@ -9,6 +9,7 @@ from image_geometry import PinholeCameraModel
 import os
 import json
 from datetime import datetime
+import rasterio
 
 bridge = CvBridge()
 
@@ -72,11 +73,48 @@ def visualize_point_clouds(ground_pc, non_ground_pc):
     o3d.visualization.draw_geometries([ground_pcd, non_ground_pcd])
 
 
-def process_oak(points):
-    ###################
-    #TODO task1
-    return points
+def process_oak(color_img_path, depth_img_path):
+    color_img = cv2.imread(color_img_path)
+    color_img = cv2.cvtColor(color_img, cv2.COLOR_BGR2RGB)
 
+    hsv = cv2.cvtColor(color_img, cv2.COLOR_RGB2HSV)
+
+    lower_bound = np.array([98, 43, 0])
+    upper_bound = np.array([118, 143, 94])
+
+    mask = cv2.inRange(hsv, lower_bound, upper_bound)
+
+    contours, _ = cv2.findContours(mask, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+
+    largest_contour = max(contours, key=cv2.contourArea) if contours else None
+
+    box_mask = np.zeros_like(mask, dtype=np.uint8)
+    if largest_contour is not None:
+        cv2.drawContours(box_mask, [largest_contour], -1, 255, thickness=cv2.FILLED)
+
+    with rasterio.open(depth_img_path) as src:
+        depth_array = src.read(1)
+
+    masked_depth = np.where(box_mask == 255, depth_array, 0)
+
+    fx, fy = 684.8333, 684.6097
+    cx, cy = 573.3711, 363.7009
+
+    points = []
+    colors = []
+    h, w = depth_array.shape
+
+    for y in range(h):
+        for x in range(w):
+            d = masked_depth[y, x]
+            if d > 0:
+                z = d * 0.1
+                X = (x - cx) * z / fx
+                Y = (y - cy) * z / fy
+                points.append([X, Y, z])
+                colors.append(color_img[y, x] / 255.0)
+
+    return points
 
 def process_ouster(points):
     ####################
